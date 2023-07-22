bbc8a3b01c3f7d164d2c407805492bda54483f4a
VictorPLopes <victor.probio@gmail.com>
2023-07-22 14:30:35 -0300

    Added README.md

--- FixGraphicEQ/FrequencyGain.cs
+++ FixGraphicEQ/FrequencyGain.cs
@@ -1,5 +1,12 @@
-﻿namespace FixGraphicEQ; 
+﻿namespace FixGraphicEQ;
 
+// Class to store frequency and gain values, simplest way I could think of to store an int with a float
 public class FrequencyGain {
-    
+    public FrequencyGain(int frequency, float gain) {
+        Frequency = frequency;
+        Gain = gain;
+    }
+
+    public int Frequency { get; set; }
+    public float Gain { get; set; }
 }
